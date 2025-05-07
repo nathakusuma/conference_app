d3c24c07647bab5a2dde63111ac929ce270716c6
nathakusuma <nathakusuma07@gmail.com>
2025-05-07 23:17:52 +0700

    feat: handle API exceptions in conference registration

--- lib/presentation/providers/registration_action_provider.dart
+++ lib/presentation/providers/registration_action_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../core/network/api_client.dart';
 import '../../domain/usecases/registrations/register_for_conference.dart';
 
 enum RegistrationStatus {
@@ -19,6 +20,14 @@ class RegistrationActionProvider with ChangeNotifier {
   RegistrationStatus get status => _status;
   String? get errorMessage => _errorMessage;
 
+  void _handleApiException(dynamic e) {
+    if (e is ApiException) {
+      _errorMessage = e.message;
+    } else {
+      _errorMessage = 'An unexpected error occurred. Please try again.';
+    }
+  }
+
   Future<bool> registerForConference(String conferenceId) async {
     _status = RegistrationStatus.loading;
     _errorMessage = null;
@@ -31,7 +40,7 @@ class RegistrationActionProvider with ChangeNotifier {
       return true;
     } catch (e) {
       _status = RegistrationStatus.error;
-      _errorMessage = e.toString();
+      _handleApiException(e);
       notifyListeners();
       return false;
     }
